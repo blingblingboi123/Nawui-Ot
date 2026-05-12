c097da055d0849984b7d2c87f09a48d64cbd8a29
blingblingboi123 <samanmawon6@gmail.com>
2026-05-12 14:14:07 +0530

    Fix Express wildcard route

--- backend/server.js
+++ backend/server.js
@@ -35,9 +35,9 @@ app.use("/api/v1/order", orderRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
+ app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 }
 
 app.listen(PORT, () => {
