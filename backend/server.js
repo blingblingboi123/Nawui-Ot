c76a1316578ba9fca27ab6f119fe9987d5581e7a
blingblingboi123 <samanmawon6@gmail.com>
2026-05-12 15:41:33 +0530

    Fix frontend path and mongodb deploy

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
-app.get("/api/health", (req, res) => {
-  res.json({ success: true, message: "Backend API working" });
-});
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
@@ -25,10 +27,12 @@ app.use("/api/v1/cart", cartRoute);
 app.use("/api/v1/order", orderRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
