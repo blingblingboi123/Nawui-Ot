407eb72f45ccb8270310b8bab2a53d1942d55938
blingblingboi123 <samanmawon6@gmail.com>
2026-05-12 14:52:56 +0530

    Fix authHeaders export

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -5,6 +5,16 @@ export const API_URL =
     ? "http://localhost:5000"
     : "";
 
+export const authHeaders = () => {
+  const token = localStorage.getItem("accessToken");
+
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+};
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || API_URL,
   withCredentials: true,
